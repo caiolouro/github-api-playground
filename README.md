4630123061acda4e84c838a36008c19bff95f1e4
Caio Louro <caio.louro@vtex.com.br>
2020-07-16 21:37:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub API Playground
+
+Solution to play around with the GitHub API using a .NET Core console application.
---
+++ github-api-playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github_api_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
