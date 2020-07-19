5f3612c71fc14000963e5a61df5d9e3565b80eba
Caio Louro <caiolouro@caio-mac-2012.local>
2020-07-18 21:38:49 -0300

    added solution structure

--- GitHubApiPlayground.Main/GitHubApiClient.cs
+++ GitHubApiPlayground.Main/GitHubApiClient.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace GitHubAPIPlayground
+namespace GitHubApiPlayground
 {
     public static class GitHubApiClient
     {
--- GitHubApiPlayground.Main/Program.cs
+++ GitHubApiPlayground.Main/Program.cs
@@ -5,7 +5,7 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace GitHubAPIPlayground
+namespace GitHubApiPlayground
 {
     class Program
     {
